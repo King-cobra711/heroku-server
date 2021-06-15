bf3d8ea0e272880fba34a21bf0b5083eae5e4eaa
Matthew Hansen <MY_NAME@example.comlearntocode711@gmail.comlscleargit config --global user.email MY_NAME@example.comlearntocode711@gmail.comclear>
2021-06-15 14:47:46 +1000

    new

--- testing/index.test.js
+++ testing/index.test.js
@@ -16,49 +16,110 @@ beforeAll(async (done) => {
   done();
 });
 
-// describe("Successful registration check with email, username and password", () => {
-//   test("Should respond with a 200 status code", async (done) => {
-//     const res = await request
-//       .post("/checkRegisterDetails")
-//       .set("Cookie", cookie)
-//       .send({
-//         Email: "jesttest@email.com.au",
-//         Username: "Jestyboy",
-//         Password: "Password",
-//       });
-//     expect(res.status).toBe(200);
-//     done();
-//   });
-// });
+describe("Successful registration check with email, username and password", () => {
+  test("Should respond with a 200 status code", async (done) => {
+    const res = await request
+      .post("/checkRegisterDetails")
+      .set("Cookie", cookie)
+      .send({
+        Email: "jesttest@email.com.au",
+        Username: "Jestyboy",
+        Password: "Password",
+      });
+    expect(res.status).toBe(200);
+    done();
+  });
+});
 
-// describe("Successful Login with email, username and password", () => {
-//   test("Should respond with a 200 status code", async (done) => {
-//     const res = await request.post("/register").set("Cookie", cookie).send({
-//       Email: "jesttestasfasd@email.com.au",
-//       Username: "Jestyboysdgfs",
-//       Password: "Password",
-//     });
-//     expect(res.statusCode).toBe(200);
-//     done();
-//   });
-// });
+describe("Successful Login with email, username and password", () => {
+  test("Should respond with a 200 status code", async (done) => {
+    const res = await request.post("/register").set("Cookie", cookie).send({
+      Email: "jesttestasfasd@email.com.au",
+      Username: "Jestyboysdgfs",
+      Password: "Password",
+    });
+    expect(res.statusCode).toBe(200);
+    done();
+  });
+});
 
-// describe("Successful Login username and password", () => {
-//   test("Should respond with a 200 status code", async (done) => {
-//     const res = await request.post("/login").set("Cookie", cookie).send({
-//       Username: "testuser",
-//       Password: "aaaaaa",
-//     });
-//     expect(res.statusCode).toBe(200);
-//     done();
-//   });
-// });
+describe("Successful Login username and password", () => {
+  test("Should respond with a 200 status code", async (done) => {
+    const res = await request.post("/login").set("Cookie", cookie).send({
+      Username: "testuser",
+      Password: "aaaaaa",
+    });
+    expect(res.statusCode).toBe(200);
+    done();
+  });
+});
 
 describe("Successful retrieval of user's scores", () => {
   test("Should respond with a 200 status code", async (done) => {
     const res = await request.post("/userScores").set("Cookie", cookie).send({
       id: 43,
     });
+    expect(res.status).toBe(200);
+    done();
+  });
+});
+
+describe("Successful update of user's Easy score", () => {
+  test("Should respond with a 200 status code", async (done) => {
+    const res = await request
+      .post("/UpdateEasyScore")
+      .set("Cookie", cookie)
+      .send({
+        id: 43,
+        score: 10,
+      });
+    expect(res.status).toBe(200);
+    done();
+  });
+});
+describe("Successful update of user's Medium score", () => {
+  test("Should respond with a 200 status code", async (done) => {
+    const res = await request
+      .post("/UpdateMediumScore")
+      .set("Cookie", cookie)
+      .send({
+        id: 43,
+        score: 15,
+      });
+    expect(res.status).toBe(200);
+    done();
+  });
+});
+describe("Successful update of user's Hard score", () => {
+  test("Should respond with a 200 status code", async (done) => {
+    const res = await request
+      .post("/UpdateHardScore")
+      .set("Cookie", cookie)
+      .send({
+        id: 43,
+        score: 20,
+      });
+    expect(res.status).toBe(200);
+    done();
+  });
+});
+describe("Successful retrieval of all user's scores", () => {
+  test("Should respond with a 200 status code", async (done) => {
+    const res = await request.get("/scores").set("Cookie", cookie);
+    expect(res.status).toBe(200);
+    done();
+  });
+});
+describe("Successful retrieval of all user's for admin manipulation", () => {
+  test("Should respond with a 200 status code", async (done) => {
+    const res = await request.get("/AdminUserSearch").set("Cookie", cookie);
+    expect(res.status).toBe(200);
+    done();
+  });
+});
+describe("Successful retrieval of all user's for admin manipulation", () => {
+  test("Should respond with a 200 status code", async (done) => {
+    const res = await request.get("/UserTypes").set("Cookie", cookie);
     expect(res.status).toBe(200);
     done();
   });
